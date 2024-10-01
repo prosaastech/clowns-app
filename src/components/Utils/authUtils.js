634ab5bdff4edd97ecdaae8f4a75cc58fda2c0eb
faisalmotan <faisal.motan3@gmail.com>
2024-10-01 19:40:22 +0500

    protected route updated

--- src/components/Utils/authUtils.js
+++ src/components/Utils/authUtils.js
@@ -1,17 +1,24 @@
 // src/utils/authUtils.js
 import { jwtDecode } from 'jwt-decode';
-
+ 
 export const getUserFromToken = () => {
   const token = localStorage.getItem('token');
   if (!token) return null;
 
   try {
     const decodedToken = jwtDecode(token);
-    //console.log('DecodedToken:', decodedToken);
-    return decodedToken.unique_name; // Assuming the token contains a `user` field
+    const currentTime = Date.now() / 1000; // Current time in seconds
+
+    // Check if the token has expired
+    if (decodedToken.exp && decodedToken.exp < currentTime) {
+      console.error('Token has expired.');
+      return null;
+    }
+
+    // Return the 'unique_name' or whichever key you want to return
+    return decodedToken.unique_name; // Adjust based on your token structure
   } catch (error) {
     console.error('Invalid token:', error);
     return null;
   }
-  
 };
